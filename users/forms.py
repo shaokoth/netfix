5db8f1ce96545775f634e9b111a19b119bcba148
shaokoth <shadrackomondi005@gmail.com>
2025-05-29 23:12:55 +0300

    Revert "chore: Update version of UserLoginForm"
    This reverts commit 0a600f6c37bb12e0b6418020ec5a97cdb2d47141.

--- users/forms.py
+++ users/forms.py
@@ -56,8 +56,14 @@ class CompanySignUpForm(UserCreationForm):
 
 
 class UserLoginForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
     email = forms.EmailField(widget=forms.TextInput(
-        attrs={'placeholder': 'Enter Email',
-               'autocomplete': 'off'}))
+        attrs={'placeholder': 'Enter Email'}))
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password'}))
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['autocomplete'] = 'off'
