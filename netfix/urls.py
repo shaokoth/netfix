74cffc1c13329ea9105c2ef3835a3a71ebf291a7
shaokoth <shadrackomondi005@gmail.com>
2025-06-05 15:25:34 +0300

    chore: Add paths for user.urls

--- netfix/urls.py
+++ netfix/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('main.urls')),
     path('services/', include('services.urls')),
     path('users/', include('users.urls')),
+    path('', include('users.urls2')),
     path('customer/<slug:name>', v.customer_profile, name='customer_profile'),
     path('company/<slug:name>', v.company_profile, name='company_profile')
 ]
