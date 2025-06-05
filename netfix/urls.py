3d36acc0662e149035261a3da55250a2de74a976
shaokoth <shadrackomondi005@gmail.com>
2025-06-05 06:06:01 +0300

    feat(forms): Add RequestServiceForm for service requests

--- netfix/urls.py
+++ netfix/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('services/', include('services.urls')),
-    path('register/', include('users.urls')),
+    path('users/', include('users.urls')),
     path('customer/<slug:name>', v.customer_profile, name='customer_profile'),
     path('company/<slug:name>', v.company_profile, name='company_profile')
 ]
